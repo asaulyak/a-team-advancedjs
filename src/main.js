3d2086bd8126f2427410f1269db58baa7514b179
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-02-21 22:36:36 +0200

    fix bugs

--- src/main.js
+++ src/main.js
@@ -1,10 +1,8 @@
 // Main entry point
 import { initBurger } from './burger/burger.js';
-import { markupCategories } from './categories-list/categories-list.js';
 
 function start() {
   initBurger();
-  markupCategories();
 }
 
 // Execution starts here
