384be82c6fefb0b2a273f42c855eb3c409a6ebf8
Stanishovskyi <arturstepanovi4@gmail.com>
2024-02-21 06:35:17 +0200

    update

---
+++ src/js/quote.js
@@ -0,0 +1,35 @@
+import APIService from '../api/api'; 
+const quoteOfTheDay = document.querySelector('клас з html'); 
+const apiService = new APIService();
+quote();
+
+async function quote() {
+  const quoteData = JSON.parse(localStorage.getItem('quoteDay')); 
+  const currentDate = new Date(); 
+  const newData = currentDate.toISOString().split('T')[0]; 
+
+  if (quoteData?.currentDate === newData) {
+    displayQuote(quoteData.data); 
+    return; 
+  }
+  
+  try {
+    const data = await apiService.getQuote();
+    const combinedData = {
+      data: data,
+      currentDate: newData,
+    }; 
+
+    localStorage.setItem('quoteDay', JSON.stringify(combinedData)); 
+    displayQuote(data);
+  } catch (error) {
+    console.log(error); 
+  }
+}
+
+function displayQuote({ author, quote }) {
+  quoteOfTheDay.innerHTML = `
+  <p class="quote__text">${quote}</p>
+  <h4 class="quote__author">${author}</h4>
+    `;
+}
