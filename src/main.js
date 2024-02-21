930e1d87d6271505f7063e53d1686337fb13e8ab
didukhroma <didukhroma@gmail.com>
2024-02-21 21:42:43 +0200

    fix: deleyted fixed function add stylization for main

--- src/main.js
+++ src/main.js
@@ -1,13 +1,10 @@
 // Main entry point
 import { initBurger } from './burger/burger.js';
-// Fixed footer
-import { initFixedFooter } from './fixedFooter/fixedFooter.js';
 // Subscribe
 import { initSubscribe } from './subscribe/subscribe.js';
 
 function start() {
   initBurger();
-  initFixedFooter();
   initSubscribe();
 }
 
