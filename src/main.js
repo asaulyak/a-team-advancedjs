49d687bd3b01ec44c65a18bd2bc046bb5e138d60
igorivnaolga <109050006+igorivnaolga@users.noreply.github.com>
2024-02-21 19:20:15 +0200

    added categories-list

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 // Main entry point
 import { initBurger } from './burger/burger.js';
+import { markupCategories } from './categories-list/categories-list.js';
 
 function start() {
   initBurger();
+  markupCategories();
 }
 
 // Execution starts here
