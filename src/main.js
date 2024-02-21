e79792e17a70ce24724c6f7b311f25a15646f57d
didukhroma <didukhroma@gmail.com>
2024-02-21 18:02:12 +0200

    feature: add subscribe logic

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@
 import { initBurger } from './burger/burger.js';
 // Fixed footer
 import { initFixedFooter } from './fixedFooter/fixedFooter.js';
+// Subscribe
+import { initSubscribe } from './subscribe/subscribe.js';
 
 function start() {
   initBurger();
   initFixedFooter();
+  initSubscribe();
 }
 
 // Execution starts here
