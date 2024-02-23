e937f39ad2798828f85c070c64276f4365407233
tatidan <TatianaD@ein-des-ein.com>
2024-02-23 20:56:45 +0100

    js logics simplified

--- src/favorites/favorites.js
+++ src/favorites/favorites.js
@@ -1,25 +1,19 @@
-// import { getExercisesById } from '../api/api';
 import { getQuote, getExercisesById } from '../api/api';
 import { storage } from '../storage/storage.js';
 
-// 1. автоматичний рендер quoteOfTheDay
+export function initFavoritesPage() {
+  const favoritesTitle = document.querySelector('.favorites-title');
 
-// функция getQuote() поверне дані для рендеру
-// можна її перший виклик додати в function start()
-// плюс запис в storage дати (наприклад, quoteTime) цього першого виклику getQuote
-// перевірка, чи пройшла доба з моменту quoteTime,
-// тоді даємо новий виклик getQuote() -> свіжа цитата
+  if (!favoritesTitle) {
+    return;
+  }
 
-// 2. автоматичний рендер блоку норми спорту
+  renderSportsNorm();
 
-const favoritesTitle = document.querySelector('.favorites-title');
-
-renderSportsNorm();
-
-function renderSportsNorm() {
-  favoritesTitle.insertAdjacentHTML(
-    'beforeend',
-    `<div class="favorites-sportsnorm-wrapper">
+  function renderSportsNorm() {
+    favoritesTitle.insertAdjacentHTML(
+      'beforeend',
+      `<div class="favorites-sportsnorm-wrapper">
       <div class="favorites-sportsnorm favorites-margin">
           <svg class= "favorites-sportsicon">
             <use href="./image/icons.svg#icon-sportsnorm"></use>
@@ -29,66 +23,42 @@ function renderSportsNorm() {
       </div>
       <div class="favorites-sportsimage favorites-margin">
       </div>`
-  );
-}
+    );
+  }
 
-// 4. динамічний рендер карток
-renderCardsSection();
+  renderCardsSection();
 
-// console.dir(storage);
+  // const exerciseId = storage.get('key');
 
-////// додавання в favorites з модального вікна - кнопка з сердечком
-////// storage.set('exerciseID', 'значення exerciseID');
+  function renderCardsSection() {
+    //   if (exerciseId) {
+    //     const dataForFavorites = getDataForFavorites(exerciseId);
+    //     renderFavoritesCards(dataForFavorites);
+    //   } else {
+    renderNoItemsMessage();
+    //   }
+  }
 
-// exerciseId = storage.get('exerciseID');
-
-// 4.2. валідація даних та сервер
-// 4.2.1 якщо exerciseId не null, то
-
-function renderCardsSection() {
-  // if (exerciseId) {
-  // const dataForFavorites = getDataForFavorites(exerciseId);
-  // renderFavoritesCards(dataForFavorites);
-  // } else {
-  renderNoItemsMessage();
+  // function getDataForFavorites(exerciseId) {
+  //   return getExercisesById(exerciseId);
   // }
+
+  // function renderFavoritesCards(dataForFavorites) {
+  //   favoritesTitle.insertAdjacentHTML(
+  //     'beforeend',
+  //     '<div><p>template favorite cards here</p></div>'
+  //   );
+  // }
+
+  function renderNoItemsMessage() {
+    favoritesTitle.insertAdjacentHTML(
+      'beforeend',
+      `<p class="favorites-noitemsmessage">It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>`
+    );
+  }
+
+  // const deleteCardBtn = document.querySelector('.favorites-iconBin');
+  // deleteCardBtn.addEventListener('click', removeFavoritesCard);
 }
 
-// 4.3. йдемо на сервер
-// function getDataForFavorites(exerciseId) {
-//   return getExercisesById(exerciseId);
-// }
-
-// 4.4.1 рендер карток
-function renderFavoritesCards(dataForFavorites) {
-  // console.log(dataForFavorites);
-  favoritesTitle.insertAdjacentHTML(
-    'beforeend',
-    '<div><p>template favorite cards here</p></div>'
-  );
-}
-
-// 4.2.2 if null,
-// 4.4.2 даємо юзеру меседж, getExercisesById не викликаємо;
-
-function renderNoItemsMessage() {
-  favoritesTitle.insertAdjacentHTML(
-    'beforeend',
-    `<p class="favorites-noitemsmessage">It appears that you haven't added any exercises to your favorites yet. To get started, you can add exercises that you like to your favorites for easier access in the future.</p>`
-  );
-}
-
-// 5. якщо карток багато, рендер скрола справа
-
-// 6. логіка видалення картки із favorites
-
-// const deleteCardBtn = document.querySelector('.favorites-iconBin');
-// deleteCardBtn.addEventListener('click', removeFavoritesCard);
-
-function removeFavoritesCard(e) {
-  console.log(e.currentTarget);
-  // беремо key&value картки, яку хочемо видалити
-  // storage.set(key, value) - тут треба перевизначити значення айді
-  // на пустий рядок, щоб картка була видалена із storage
-  // renderFavoritesCards() // тут будуть всі картки зі Storage, окрім видаленої
-}
+// const favoritesTitle = document.querySelector('.favorites-title');
--- src/main.js
+++ src/main.js
@@ -5,26 +5,25 @@ import { headerHighlighting } from './header-highlighting/header-highlighting.js
 
 // Subscribe
 import { initSubscribe } from './subscribe/subscribe.js';
+// Subscribe
+import { initFavoritesPage } from './favorites/favorites.js';
 // Categories List
 import './categories-list/categories-list.js';
 //Modal Exercises
 import './modal/modal-exercises.js';
-
-//Favorites
-import './favorites/favorites.js';
 
 // Scroll up button
 import { initScrollUp } from './scroll-up/scroll-up.js';
 
 import { renderExerciseList } from './exerciseList/exerciseList';
 
-
 function start() {
   initBurger();
   headerHighlighting();
   initSubscribe();
   renderExerciseList(); // TODO: Move to the corresponding parent component
-  initScrollUp();
+  // initScrollUp();
+  initFavoritesPage();
 }
 
 // Execution starts here
