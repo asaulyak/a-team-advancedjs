cb2a5a4342eafdc160b83d063364ade58b565e2d
Roman <140334590+RomanS1994@users.noreply.github.com>
2024-02-23 21:39:44 +0100

    added localStorage

--- src/filter_panels/filter_panels.js
+++ src/filter_panels/filter_panels.js
@@ -10,13 +10,13 @@ const section = document.querySelector('.filter_panel');
 const exercises_title = `<h2 class="exercises-title">Exercises</h2>`;
 const form_buttons = `
 <div class="category-elements">
-  <form class="search-form visually-hidden">
+  <div class="search-form visually-hidden">
     <input class="form-input" type="text" placeholder="Search" />
     <button class="search-button"type="button"aria-label="search button">
       <img class="search-icon" src="../image/icons_filter_panels/search.svg" alt="icon">
       <img class="close-icon visually-hidden" src="../image/icons_filter_panels/x.svg" alt="icon">
     </button>
-  </form>
+  </div>
   <ul class="category-list_buttons">
     <li><button class="category-button "type="button" id="Muscles"aria-label="category button">Muscles</button></li>
     <li><button class="category-button " type="button" id="Body parts"aria-label=" category button">Body parts</button></li>
@@ -39,7 +39,9 @@ export function getFilterPanels() {
     input: document.querySelector('.form-input'),
     searchButton: document.querySelector('.search-button'),
   };
+
   const { title, list, input, searchButton, form, categoryButtons } = elements;
+
   const searchAndCloseIcons = {
     searchIcon: document.querySelector('.search-icon'),
     closeIcon: document.querySelector('.close-icon'),
@@ -55,14 +57,27 @@ export function getFilterPanels() {
     }
   });
 
-  getSubCategoriesByCategory({ category: storageValue }).then(
-    ({ data: { results } }) => {
-      const marcupCategoryList = markupCategories(results); //виклик функції для рендеру підкатегорій
-      const categoriesList = document.querySelector('.categories-list');
-      categoriesList.innerHTML = marcupCategoryList;
-      console.log(results);
-    }
-  );
+  if (storage.get('categori')) {
+    getExercisesByCategory({
+      keyword: valueInput,
+      category: StorageCategory,
+      subCategory: storage.get('category'), //'abs'
+    }).then(({ data: { results } }) => {
+      /************ потрібен експорт функції від Андрія ************/
+      // const marcupExercises = createBlockMarkupArr(results);
+      // const exerciseSection = document.querySelector('.exercise-section');
+      // exerciseSection.innerHTML = marcupExercises;
+      // cl-data);
+    });
+  } else if (storageValue) {
+    getSubCategoriesByCategory({ category: storageValue }).then(
+      ({ data: { results } }) => {
+        const marcupCategoryList = markupCategories(results);
+        const categoriesList = document.querySelector('.categories-list');
+        categoriesList.innerHTML = marcupCategoryList;
+      }
+    );
+  }
   //---------------------------------------------
 
   list.addEventListener('click', handleClickCategory);
@@ -94,8 +109,10 @@ function handleClickCategory(e) {
   const form = document.querySelector('.search-form');
   const storageValue = storage.get('filter');
 
-  // form.classList.remove('visually-hidden');
+  form.classList.remove('visually-hidden');
   storage.set('filter', e.target.id);
+  storage.remove('category');
+
   getSubCategoriesByCategory({ category: e.target.id }).then(
     ({ data: { results } }) => {
       const marcupCategoryList = markupCategories(results); //виклик функції для рендеру підкатегорій
@@ -104,14 +121,13 @@ function handleClickCategory(e) {
 
       categoryButtons.forEach(button => button.classList.remove('selected'));
       e.target.classList.add('selected');
-      console.log(results);
     }
   );
 }
 
 export function onShowSearchForm() {
   const form = document.querySelector('.search-form');
-  form.classList.remove('visually-hidden'); //показ форми(змінити на add) (викликати A.B)
+  form.classList.add('visually-hidden'); //показ форми(змінити на add)
 }
 
 function handlerSubmit(e) {
@@ -132,16 +148,16 @@ function handlerSubmit(e) {
       : storage.get('filter') === 'Body parts'
         ? 'bodypart'
         : 'equipment';
-
-  //Потрібно записати вибрану частину тіла в LS.
   valueInput !== '' &&
     getExercisesByCategory({
       keyword: valueInput,
       category: StorageCategory,
-      subCategory: storage.get('subCategory'), //'abs'
+      subCategory: storage.get('category'),
     }).then(({ data }) => {
-      //Виклик функції для рендеру вправ по ключовому слові A.B
+      /************ потрібен експорт функції від Андрія  ************/
+      // const marcupExercises = createBlockMarkupArr(results);
+      // const exerciseSection = document.querySelector('.exercise-section');
+      // exerciseSection.innerHTML = marcupExercises;
       // cl-data);
-      console.log(data);
     });
 }
